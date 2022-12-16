234f96d4cc33a36e33caa28057c04ba62f6c74f1
Ekaterina_P <zazie25@yandex.ru>
2022-12-16 21:14:13 +0300

    add Task 04

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -14,6 +14,5 @@ int max = number1;
 if (number1>max ) max = number1;
 if (number2>max ) max = number2;
 if (number3>max ) max = number3;
-
 Console.Write("max = ");
-Console.Write(max);
+Console.Write (max);
