d33cb6aeee9a35ea942c7ae1686efd49a45ba5de
Ekaterina_P <zazie25@yandex.ru>
2022-12-24 17:17:37 +0300

    add Task 04

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -11,7 +11,6 @@ Console.Write("Введите третье число: ");
 int number3 = Convert.ToInt32(Console.ReadLine());
 int max = number1;
 
-if (number1>max ) max = number1;
 if (number2>max ) max = number2;
 if (number3>max ) max = number3;
 {
