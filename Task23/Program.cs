a0a9647af8fd30f58840e090de39f61e24361342
Ekaterina_P <zazie25@yandex.ru>
2022-12-24 16:52:55 +0300

    add Task 23

---
+++ Task23/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая 
+// 1. принимает на вход число (N) и 
+// 2. выдаёт таблицу кубов чисел от 1 до N.
+// 5 -> 
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+Console.Write("Введите натуральное число N:   ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    TableSquare(number);
+}
+else Console.WriteLine("Не натуральное число");
+void TableSquare(int num)
+{
+    int index = 1;
+    while (index <= num)
+    {
+        Console.WriteLine($"{index, 4} {index * index * index, 15}");
+        index++;
+    }
+}
