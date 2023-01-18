9429f83b249ff79d1e863b544c3af9c57abfa130
Ekaterina_P <zazie25@yandex.ru>
2023-01-18 21:53:08 +0300

    add Task 29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -16,14 +16,19 @@ int[] CreateArray(int size)
     }
     return arr;
 }
-
 void PrintArray(int[] arr)
 {
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
+        else Console.Write(arr[i]);
+    }
+    Console.Write(" -> ");
     Console.Write("[");
     for (int i = 0; i < arr.Length; i++)
     {
         if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
         else Console.Write(arr[i]);
     }
-    Console.WriteLine("]");
+    Console.Write("]");
 }
