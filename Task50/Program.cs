7ff1f86e85e6ecdbff57f518d2d3e2f2490edd96
Ekaterina_P <zazie25@yandex.ru>
2023-01-31 23:25:56 +0300

    Task 50

--- Task50/Program.cs
+++ Task50/Program.cs
@@ -50,3 +50,61 @@ void PrintMatrix(int[,] matrix)
         Console.WriteLine("]");
     }
 }
+//   Вариант с переменной в цикле. 
+
+// int[,] matrix = CreateMatrixRndInt(3, 4, -10, 10);
+// TestNatural(matrix);
+// PrintMatrix(matrix);
+
+// void TestNatural (int[,] matrix)
+// { 
+//     while (true)
+//     {
+//         Console.WriteLine("Введите номер строки: ");
+//         int row = Convert.ToInt32(Console.ReadLine());
+//         if (row <= 0) Console.WriteLine("Введите число больше 0");
+//         else break;
+//     }
+//     while (true)
+//         {
+//             Console.WriteLine("Введите номер столбца: ");
+//             int column = Convert.ToInt32(Console.ReadLine());
+//             if (column <= 0) Console.WriteLine("Введите число больше 0");
+//             else break;
+//             // return column;
+//             // CreateMatrixRndInt(8, 7, 1, 50); 
+//         }
+// }
+//     if (row <= matrix.GetLength(0) + 1 && column <= matrix.GetLength(1) + 1)
+//                 Console.WriteLine($"строка {row}, столбец {column} -> {matrix[row - 1, column - 1]}");
+
+//             else Console.WriteLine("И номер строки, и номер столбца должны быть больше 0.");
+
+// int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+// {
+//     int[,] matrix = new int[rows, columns];
+//     Random rnd = new Random();
+
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = rnd.Next(min, max + 1);
+//         }
+//     }
+//     return matrix;
+// }
+
+// void PrintMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         Console.Write("[");
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5},");
+//             else Console.Write($"{matrix[i, j],5}  ");
+//         }
+//         Console.WriteLine("]");
+//     }
+// }
